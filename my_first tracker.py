eafb99c565e9dbe965b25736391e7dcbfa320b14
dev123-hub <ldev94089@gmail.com>
2025-06-09 09:23:20 +0530

    first use of variabls

--- my_first tracker.py
+++ my_first tracker.py
@@ -1,2 +1,11 @@
 print("Welcome to my Finance Tracker!")
 print("Let's learn Python together!")
+
+# Variables - like labeled boxes to store information
+my_name = "Dev L"  # Put your actual name!
+starting_money = 1000
+coffee_price = 4.50
+
+print("Hello", my_name)
+print("You start with $", starting_money)
+print("Coffee costs $", coffee_price)
